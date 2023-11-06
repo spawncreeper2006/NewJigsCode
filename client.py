f50aa9b7f07175e32b1fa55422d8043a3d8152e0
spawncreeper2006 <97362498+spawncreeper2006@users.noreply.github.com>
2023-11-06 21:54:20 +0000

    first commit

---
+++ client.py
@@ -0,0 +1,26 @@
+
+
+    # def openarchive(event):
+    #     subprocess.run(["python","text viewer"])
+    #     pass
+        
+
+
+    # def proccessing(event):
+    #     time = datetime.datetime.now()
+    #     file = open(username,"_",destination_user,time)
+    #     timestamp = ("<",time,">")
+    #     finished = (timestamp, " User[",username,"]","\n")
+    #     userentry = entry.get()
+    #     message = ("\n",str(finished),"\n",str(userentry))
+    #     print(message)
+    #     file = open(username+"_"+destination_user+".txt","a")
+    #     global filename 
+    #     filename = (username+"_"+destination_user+".txt")
+    #     file.writelines(message)
+    #     file.close()
+    #     run_encryption()
+    #     run_client(filename)
+
+
+
---
+++ user_interface.py
@@ -0,0 +1,124 @@
+from tkinter import *
+from tkinter import ttk
+
+CONTACT_WINDOW_DIMENSIONS = (400, 200)
+MESSAGE_WRITE_WINDOW_DIMENSIONS = (400, 250)
+
+
+message = ''
+button_pressed = ''
+
+
+def ContactsWindow(contacts: list) -> str:
+    '''Prompts the user to choose a contact and returns selected.'''
+
+
+    root = Tk()
+    root.title("Contact Selection")
+    root.geometry(f"{CONTACT_WINDOW_DIMENSIONS[0]}x{CONTACT_WINDOW_DIMENSIONS[1]}")
+
+
+
+
+    clicked=StringVar()
+    Button( root , text = "Compose Message", command = lambda: label.config( text = clicked.get() )).pack()
+    label = Label( root , text = " ")
+    label.pack()
+
+    clicked.set( contacts[0] )
+
+    drop = OptionMenu(root , clicked , *contacts)
+    drop.pack()
+
+    root.mainloop()
+
+def MessageWriteWindow() -> (str, str):
+    '''Asks the user to input a message and returns a tuple with archive / send as 0th element and the message as the second element.'''
+
+    global message, button_pressed
+    message = ''
+    button_pressed = ''
+
+    root = Tk()
+
+    root.title("New Message")
+    root.geometry(f"{MESSAGE_WRITE_WINDOW_DIMENSIONS[0]}x{MESSAGE_WRITE_WINDOW_DIMENSIONS[1]}")
+    root.resizable(False,False)
+
+
+    textbox = Text(root, width=20, height=5)
+    textbox.place(x=120, y=0)
+
+
+    def archive_action():
+        global message, button_pressed
+        message = textbox.get("1.0","end-1c")
+        button_pressed = 'archive'
+        root.destroy()
+
+    def send_action():
+        global message, button_pressed
+        message = textbox.get("1.0","end-1c")
+        button_pressed = 'send'
+        root.destroy()
+        
+
+
+    Button(root, text="Archived Messages", command=archive_action).place(x=50, y=200)
+    Button(root, text = 'Send Message', command=send_action).place(x=200, y=200)
+
+
+
+
+
+
+    root.mainloop()
+
+    return (button_pressed, message)
+
+
+
+def MessageViewerWindow(messages: list):
+    root = Tk()
+    root.title('Message Viewer')
+    root.resizable(False,False)
+
+    main_frame = Frame(root)
+    main_frame.pack(fill = BOTH, expand=1 )
+
+    canvas = Canvas(main_frame)
+    canvas.pack(side = LEFT, fill = BOTH, expand=1 )
+
+
+
+    # #create scroll bars
+    # horizon = ttk.Scrollbar(main_frame, orient=HORIZONTAL, command=canvas.xview)
+    # horizon.pack(side = RIGHT, fill=X)
+    # vertical = ttk.Scrollbar(main_frame, orient=VERTICAL, command = canvas.yview)
+    # vertical.pack(side = RIGHT, fill=Y)
+
+    # #configure canvas things you know how it is
+    # canvas.configure(xscrollcommand = horizon.set)
+    # canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion = canvas.bbox("all")))
+    # canvas.configure(yscrollcommand=vertical.set)
+    # canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion = canvas.bbox("all")))
+
+    # frame = Frame(canvas)
+
+    # canvas.create_window((0,0), window=frame, anchor="nw")
+
+
+    # file = open(username+"_"+destination_user+".txt","r")
+    # global filename
+    # filename = (username+"_"+destination_user+".txt")
+    # contents = file.read()
+    # file.close()
+    # canvas.create_text(1, 1, text=contents , anchor='nw', font='TkMenuFont', fill='black')
+
+
+
+    canvas.create_text(1, 1, text='\n'.join(messages) , anchor='nw', font='TkMenuFont', fill='black')
+
+    root.mainloop()
+
+print (MessageViewerWindow(["a", "b"]))
